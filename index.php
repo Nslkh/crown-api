8749c4abde9a7f2460f4cec5853e20f170201579
Nslkh <olluzran@gmail.com>
2021-03-07 03:48:19 +0500

    Getting data from DB

--- index.php
+++ index.php
@@ -2,9 +2,15 @@
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
 
-$data = json_decode(file_get_contents('mock.json'), true);
+$connection = mysqli_connect($host = 'localhost', $user = 'root', $password = null, $database = 'crown_api', $port = 3306);
+$sql = "SELECT * FROM categories";
+$query = mysqli_query($connection, $sql);
+$data = mysqli_fetch_all($query, MYSQLI_ASSOC);
+
+// $data = json_decode(file_get_contents('mock.json'), true);
 
 // echo "<pre>";
 // var_dump($data);
 // echo "</pre>";
+
 echo json_encode($data, JSON_PRETTY_PRINT);
